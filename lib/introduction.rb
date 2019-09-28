4859c281d3814432262aeb87415ce56d4af4ad5f
interactive-cache-8375 <randomblue1@yahoo.com>
2019-09-28 21:06:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,14 @@
 def introduction(name) 
-  hi my name is#{name} 
+  hi my name is #{name} 
 end 
+
+def introduction_with_language (name,language)
+"hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+def introduction_with_language_optional (name,ruby)
+"hi, my name is #{name} and I am learning to program in #{language}"
+end
 
 
 
